3b605ad5436fdc1bcb51f02279c875959f5cfa32
Uchenna77-dev <uchennaonuorah40@gmail.com>
2025-09-16 05:08:04 +0100

    Added Auth

--- nextjs-dashboard/app/lib/data.ts
+++ nextjs-dashboard/app/lib/data.ts
@@ -24,7 +24,8 @@ export async function fetchRevenue() {
     // console.log('Data fetch completed after 3 seconds.');
 
     return data;
-  } catch (error) {
+  } 
+  catch (error) {
     console.error('Database Error:', error);
     throw new Error('Failed to fetch revenue data.');
   }
